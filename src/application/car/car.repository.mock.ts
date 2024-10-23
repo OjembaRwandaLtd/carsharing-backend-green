b25b0a47b8582a565b0bb75abbfbc7aaeb8376c9
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-23 11:48:15 +0200

    fix: getOne and getAll endpoint

--- src/application/car/car.repository.mock.ts
+++ src/application/car/car.repository.mock.ts
@@ -6,7 +6,7 @@ export function mockCarRepository(): CarRepositoryMock {
   return {
     findByLicensePlate: jest.fn(),
     get: jest.fn(),
-    find:jest.fn(),
+    find: jest.fn(),
     getAll: jest.fn(),
     insert: jest.fn(),
     update: jest.fn(),
