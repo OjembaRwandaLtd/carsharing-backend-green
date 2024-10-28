5890b5b77e17b5045704db37d82931c6acf78596
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-10-28 15:59:55 +0200

    perf: caching requests

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,3 +1,4 @@
+import { CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
 import { ConfigModule as NestjsConfigModule } from '@nestjs/config'
 
@@ -6,6 +7,11 @@ import { ControllerModule, UtilityModule } from './module'
 @Module({
   imports: [
     NestjsConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({
+      max: 50,
+      ttl: 0,
+      isGlobal: true,
+    }),
     UtilityModule,
     ControllerModule,
   ],
