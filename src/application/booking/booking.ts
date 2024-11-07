fba2169ad5f0ce60f1ac3fa81bbab1b24e731673
Beccatoni <rbccmtni1@gmail.com>
2024-11-07 12:26:18 +0200

    feat: add bookings

---
+++ src/application/booking/booking.ts
@@ -0,0 +1,60 @@
+import { IsInt, IsPositive, IsEnum, IsNotEmpty, IsDate } from "class-validator";
+import {type Opaque} from 'type-fest'
+import { validate } from "src/util";
+import { type UserID } from "../user";
+import { type CarID } from "../car";
+import { BookingState } from "./booking-state";
+
+
+export type BookingID = Opaque<number, 'booking-id'>
+
+export type BookingProperties = {
+    id: BookingID
+    startDate: Date
+    endDate: Date
+    carId: CarID
+    renterId: UserID
+    ownerId: UserID
+    state: BookingState
+}
+
+
+export class Booking {
+    @IsInt()
+  @IsPositive()
+  public readonly id: BookingID
+
+  @IsDate()
+  public readonly startDate: Date
+
+  @IsDate()
+  public readonly endDate: Date
+
+  @IsInt()
+  @IsPositive()
+  public readonly carId: CarID
+
+  @IsInt()
+  @IsPositive()
+  public readonly renterId: UserID
+
+  @IsInt()
+  @IsPositive()
+  public readonly ownerId: UserID
+
+  @IsEnum(BookingState)
+  public readonly state: BookingState
+
+  public constructor(data: BookingProperties){
+    this.id = data.id
+    this.startDate = data.startDate
+    this.endDate = data.endDate
+    this.carId = data.carId
+    this.renterId = data.renterId
+    this.ownerId = data.ownerId
+    this.state = data.state
+
+
+    validate(this)
+  }
+}
