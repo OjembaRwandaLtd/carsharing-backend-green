51217f84d37567b3e6aa1a25c4774c182a5fcc15
Angekarara <mbabange2020@gmail.com>
2024-11-14 14:49:08 +0200

    fix: fix booking tests

--- src/application/booking/booking.ts
+++ src/application/booking/booking.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsPositive, IsEnum, IsNotEmpty, IsDate } from 'class-validator'
+import { IsInt, IsPositive, IsEnum, IsDate } from 'class-validator'
 import { type Opaque } from 'type-fest'
 
 import { validate } from 'src/util'
