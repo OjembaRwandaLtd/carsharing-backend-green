51b17bcb29abb00a3196396b06d258557d430898
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-11-26 15:20:01 +0200

    feat: create user interface

--- src/application/user/user.repository.interface.ts
+++ src/application/user/user.repository.interface.ts
@@ -13,4 +13,6 @@ export abstract class IUserRepository {
   public abstract get(tx: Transaction, id: UserID): Promise<User>
 
   public abstract getAll(tx: Transaction): Promise<User[]>
+
+  public abstract create(tx: Transaction, user: User): Promise<User>
 }
