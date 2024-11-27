992b03766557c979eb76d188f08b1b64be0fe460
deborah <deborahkamikazi9@gmail>
2024-11-27 14:34:40 +0200

    fix : github errors

--- src/application/booking/booking-not-found.error.ts
+++ src/application/booking/booking-not-found.error.ts
@@ -5,7 +5,7 @@ import { type BookingID } from './booking'
 export class BookingNotFoundError extends NotFoundError<BookingID> {
   public constructor(bookingId?: BookingID) {
     if (bookingId === undefined) {
-      throw new Error('BookingId is undefined.');
+      throw new Error('BookingId is undefined.')
     }
     super('Booking not found', bookingId)
   }
